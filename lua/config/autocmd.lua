e41d419e2379f0da094bbba30ef17ae3e8c4adf5
Kiryl Zhukau <kiryl.zhukau@emerson.com>
2025-01-07 16:22:48 +0100

    - Removed auto format on save.

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,24 +6,24 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('lsp-attach-format-on-save', {}),
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if not client then return end
-
-        ---@diagnostic disable-next-line: param-type-mismatch
-        if client.supports_method('taxtDocument/farmatting', 0) then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                buffer = args.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-                end,
-            })
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--     group = vim.api.nvim_create_augroup('lsp-attach-format-on-save', {}),
+--     callback = function(args)
+--         local client = vim.lsp.get_client_by_id(args.data.client_id)
+--         if not client then return end
+--
+--         ---@diagnostic disable-next-line: param-type-mismatch
+--         if client.supports_method('taxtDocument/farmatting', 0) then
+--             -- Format the current buffer on save
+--             vim.api.nvim_create_autocmd('BufWritePre', {
+--                 buffer = args.buf,
+--                 callback = function()
+--                     vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+--                 end,
+--             })
+--         end
+--     end,
+-- })
 
 vim.api.nvim_create_autocmd("TermOpen", {
     group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
