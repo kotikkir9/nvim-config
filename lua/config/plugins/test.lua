d335847b989709a4318bf0727bc4689fa0411c34
Kiryl Zhukau <kiryl.zhukau@emerson.com>
2025-01-08 14:38:50 +0100

    - Added numToStr/Comment plugin.

--- lua/config/plugins/test.lua
+++ lua/config/plugins/test.lua
@@ -1,5 +1,9 @@
 return {
     {
+        'numToStr/Comment.nvim',
+        opts = {}
+    },
+    {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {
